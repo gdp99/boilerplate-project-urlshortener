dddde73bd864848d7587e2a6b604e10239ad2b35
gdp99 <giuseppedp99@gmail.com>
2025-02-11 19:14:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
+const bodyParser = require('body-parser');
+const validUrl = require('is-valid-http-url');
+const mongoose = require('mongoose');
+const shortId = require('shortid');
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  }
+)
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let urlSchema = new mongoose.Schema({
+  original_url : String,
+  short_url : String,
+})
+
+let URL = mongoose.model('URL', urlSchema);
+
+URL.createCollection();
+
 
 app.use(cors());
 
@@ -18,6 +42,31 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl/', (req,res)=>{
+  const original_url = req.body.url;
+  const short_url = shortId.generate();
+  if(validUrl(original_url)){
+    const url = new URL({
+      original_url: original_url,
+      short_url: short_url,
+    })
+    url.save();
+    res.json(url);
+  }
+  else {
+    res.json({ error: 'invalid url' })
+  }
+})
+
+app.get('/api/shorturl/:short_url', (req,res)=>{
+  console.log(req.params.short_url)
+  const short_url = req.params.short_url;
+  URL.findOne({short_url: short_url}, (err,data)=>{
+    return res.redirect(data.original_url);
+  })
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
